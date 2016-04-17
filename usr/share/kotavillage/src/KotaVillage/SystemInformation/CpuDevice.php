503179d1ecaeeef0e7c04ae69d12f56eace090a1
malaynymous <malaynymous@gmail.com>
2016-04-17 21:10:37 +0800

    Create CpuDevice.php

---
+++ usr/share/kotavillage/src/KotaVillage/SystemInformation/CpuDevice.php
@@ -0,0 +1,31 @@
+<?php
+namespace KotaVillage\SystemInformation;
+
+    /* Copyright 2008 Mohd Khazee */
+
+/*  This file is part of KOTAVILLAGE Hotspot.
+
+    http://kotavillagehotspot.tk/
+
+    KOTAVILLAGE Hotspot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KOTAVILLAGE Hotspot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GRASE Hotspot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class CpuDevice
+{
+    public $load;
+    public $speed;
+    public $model;
+    public $cache;
+    public $bogomips;
+}
