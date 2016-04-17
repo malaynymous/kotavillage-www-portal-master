968c14d84f33d7b800be35946281c9717005db36
malaynymous <malaynymous@gmail.com>
2016-04-17 22:20:16 +0800

    Update Database.php

--- usr/share/kotavillage/src/KotaVillage/Database/Database.php
+++ usr/share/kotavillage/src/KotaVillage/Database/Database.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright 2008-2014 Mohd Khazee */
+/* Copyright 2016-2018 Mohd Khazee */
 
 /*  This file is part of KOTAVILLAGE Hotspot.
 
@@ -16,7 +16,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with GRASE Hotspot.  If not, see <http://www.gnu.org/licenses/>.
+    along with KOTAVILLAGE Hotspot.  If not, see <http://www.gnu.org/licenses/>.
 */
 namespace KotaVillage\Database;
 
